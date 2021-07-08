094249c75fc72189d53c1407a4d2d5de0453c9ad
igorocky <123@gmail.com>
2021-07-08 21:07:51 +0200

    Implement validation om image hashes.

--- src/main/be-ts/config.ts
+++ src/main/be-ts/config.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import util from 'util'
-import path from "path";
+import path from 'path'
+import crypto from 'crypto'
+import {getBook} from "./data";
 
 export {loadAppConfig, appConfig}
 
@@ -13,6 +15,8 @@ interface BookMarkupConfig {
     title: string,
     bookFile: string,
     imgDir: string,
+    imgHash:string,
+    actualImgHash:string,
     selectionsFile: string
 }
 
@@ -34,4 +38,39 @@ async function loadAppConfig(configFilePath:string): Promise<void> {
         imgDir: toAbsolutePath(m.imgDir),
     }))
     appConfig.markupsById = appConfig.markups.reduce((prev,curr)=>({...prev,[curr.id]:curr}), {})
+    await validateHashes(appConfig.markups)
+}
+
+async function validateHashes(markups:BookMarkupConfig[]) {
+    const hashes = await Promise.all(
+        markups.map(markupCfg => calculateHash({markupCfg}))
+    )
+    for (let i = 0; i < markups.length; i++) {
+        markups[i].actualImgHash = hashes[i]
+    }
+    const mismatches = markups.filter(m => m.imgHash !== m.actualImgHash)
+    if (mismatches.length) {
+        const mismatchesStr = mismatches.map(m=>`id = ${m.id}\nexpected = ${m.imgHash}\nactual = ${m.actualImgHash}`).join('\n\n')
+        throw new Error(`There are mismatches in image hashes:\n${mismatchesStr}`)
+    }
+}
+
+async function calculateHash({filePath, markupCfg}: { filePath?: string, markupCfg?: BookMarkupConfig }): Promise<string> {
+    if (filePath) {
+        return new Promise(function (resolve, reject) {
+            const hash = crypto.createHash('md5')
+            const input = fs.createReadStream(filePath)
+            input.on('error', reject)
+            input.on('data', chunk => hash.update(chunk))
+            input.on('close', () => resolve(hash.digest('hex')))
+        })
+    } else if (markupCfg) {
+        const book = await getBook({bookId: markupCfg.id})
+        return Promise.all(
+            book.pages.map(page => calculateHash({filePath:path.resolve(markupCfg.imgDir, page.fileName)})) as Promise<string>[]
+        )
+            .then(pageHashes => `[${pageHashes.join(',')}]`)
+    } else {
+        throw new Error('Either filePath or markupCfg or appConfig should be specified.')
+    }
 }
