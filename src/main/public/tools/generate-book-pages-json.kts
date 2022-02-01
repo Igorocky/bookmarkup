40ec06cf12b752a2f295201374e086fa274c02b0
igorocky <123@gmail.com>
2022-02-01 21:42:03 +0100

    Update generate-book-pages-json.kts.

--- src/main/public/tools/generate-book-pages-json.kts
+++ src/main/public/tools/generate-book-pages-json.kts
@@ -2,8 +2,9 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-val dirWithImages = File("C:\\igye\\books\\tmp")
-val fileToSaveTo = File("C:\\igye\\books\\tmp\\basic-trig-identities.json")
+val projName = "basic-trig-identities"
+val dirWithOriginalImages = File("C:\\igye\\books\\tmp")
+val outputDir = File("C:\\igye\\books\\tmp\\basic-trig-identities-prepared")
 val width: Int = 2550
 val height: Int = 3299
 val cropLeft: Int = (width - 2166)
@@ -11,8 +12,15 @@ val cropRight: Int = (width - 2294)
 val cropTop: Int = (height - 2860)
 val cropBottom: Int = (height - 2984)
 
-val dirWithCroppedImages = File(dirWithImages, "cropped")
+outputDir.mkdirs()
+
+val dirWithCroppedImages = File(outputDir, projName + "-img")
 dirWithCroppedImages.mkdirs()
+
+val pagesFile = File(outputDir, "$projName.json")
+val repeatGroupsFile = File(outputDir, "$projName-repeat-groups.json")
+val selectionsFile = File(outputDir, "$projName-selections.json")
+val projConfigFile = File(outputDir, "config-content.json")
 
 val pageNumberRegex = "^.+-(\\d+)\\.png$".toRegex()
 data class PageDto(val fileName: String, val width: Int, val height: Int, val pageNum: Int) {
@@ -21,7 +29,7 @@ data class PageDto(val fileName: String, val width: Int, val height: Int, val pa
     }
 }
 
-val jsonContent = dirWithImages.listFiles().asSequence()
+val jsonContent = dirWithOriginalImages.listFiles().asSequence()
     .filter { it.name.endsWith(".png") }
     .map {origImgFile ->
         println("processing - ${origImgFile.name}")
@@ -43,4 +51,31 @@ val jsonContent = dirWithImages.listFiles().asSequence()
         separator = ",\n",
         postfix = "\n    ]\n}"
     )
-fileToSaveTo.writeText(jsonContent)
+pagesFile.writeText(jsonContent)
+
+repeatGroupsFile.writeText("""
+    {
+        "COLLECTIONS": [],
+        "SELECTED_COLLECTION_IDX": 0
+    }
+""".trimIndent())
+
+selectionsFile.writeText("[]")
+
+projConfigFile.writeText("""
+    {
+      "environmentName": "PROD",
+      "markups": [
+        {
+          "id": "$projName",
+          "title": "",
+          "bookFile": "$projName.json",
+          "imgDir": "$projName-img",
+          "imgHash": "[]",
+          "selectionsFile": "$projName-selections.json",
+    	  "repeatGroupsFile": "$projName-repeat-groups.json",
+    	  "defaultTags": []
+        }
+      ]
+    }
+""".trimIndent())
